0c4b73d1d70b3bb5e093d7f1f8cd232de23ba661
Teo Patrosio <therese.patrosio@gmail.com>
2022-11-03 07:42:39 -0700

    fixed visuals

--- src/laser_range_visualization.py
+++ src/laser_range_visualization.py
@@ -12,7 +12,7 @@ from sensor_msgs.msg import LaserScan
 # from scipy.signal import convolve as scipy_convolve
 # from astropy.convolution import convolve
 
-from sklearn.impute import KNNImputer
+# from sklearn.impute import KNNImputer
 
 # def plot_x(msg):
 #     global counter
@@ -26,12 +26,38 @@ from sklearn.impute import KNNImputer
 
 #     counter += 1
 
+def smooth(msg, radius):
+    data = np.array(msg.ranges)
+    data_copy = data
+    nan_indicies = np.argwhere(np.isnan(data))
+    for index in nan_indicies:
+        index = index[0]
+        imin = max(0, index - radius)
+        imax = min(len(data), index + radius + 1)
+        # print(type(data))
+        # print(imin, imax)
+        # print(index)
+        temp_sqr = data[imin:imax]
+        temp_sqr = temp_sqr[~np.isnan(temp_sqr)]
+        try: 
+            data_copy[index] = np.mean(temp_sqr)
+        except:
+            data_copy[index] = msg.range_max
+        # print(temp_sqr.shape)
+        # for j in temp_sqr:
+        #     if (temp_sqr[j] < 0):
+    return data_copy
+
+
 def plotter(msg):
     theta = np.linspace(msg.angle_min, msg.angle_max, 662)
     # print(theta)
     ranges = np.array(msg.ranges)
-    imputer = KNNImputer(n_neighbors=2)
-    imputer.fit_transform(ranges)
+    new_data = smooth(msg, 3)
+    # print(np.isnan(new_data))
+
+    # imputer = KNNImputer(n_neighbors=2)
+    # imputer.fit_transform(ranges)
     # ranges[np.isnan(ranges)] = 1
 
     # kernel = Gaussian1DKernel(1)
@@ -40,9 +66,11 @@ def plotter(msg):
 
     # print(ranges)
     plt.clf()
-    plt.polar(theta, ranges)
+    plt.polar(theta, new_data)
     plt.draw()
     plt.pause(0.00000000001)
+    plt.ion()
+    plt.show()
     # plt.savefig('msg_visual4.png')
 
 
@@ -54,10 +82,9 @@ if __name__ == '__main__':
     except:
         pass
     # lidar_data = rospy.wait_for_message("/base_scan", LaserScan)
-    rospy.Subscriber("/base_scan", LaserScan, plotter)
+    rospy.Subscriber("/base_scan", LaserScan, plotter, queue_size=5)
     # print(msg)
-    plt.ion()
-    plt.show()
+    rospy.sleep(0.01)
     rospy.spin()
 
 
